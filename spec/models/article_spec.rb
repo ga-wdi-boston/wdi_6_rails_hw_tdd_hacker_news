eb2ac93e65920e2ad8953aaef14b97e22b283f54
emreiser <emilie.reiser@gmail.com>
2014-02-10 18:48:08 -0500

    Added article model specs to test validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,4 +6,13 @@ describe Article do
 		it { should belong_to :user}
 	end
 
+	describe 'validations' do
+		it 'should be invalid if no user_id present' do
+			expect(Article.create(title: 'Article', url: 'www.article.com')).to_not be_valid
+			expect(Article.create(title: 'Article', user_id: 1)).to_not be_valid
+			expect(Article.create(user_id: 1, url: 'www.article.com')).to_not be_valid
+		end
+	end
+
+
 end
