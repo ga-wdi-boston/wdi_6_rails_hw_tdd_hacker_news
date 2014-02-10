28b03743ef600e8c733f165ec63c24faec7e5a2d
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-10 18:56:46 -0500

    put in a validation spec for the model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
 
 describe User do
   describe "associations" do
   	it { have_many :articles }
   	it { have_many :comments }
   end
+
+  it "is invalid without an email or password" do
+  	expect(User.create()).to_not be_valid
+  end
+
 end
