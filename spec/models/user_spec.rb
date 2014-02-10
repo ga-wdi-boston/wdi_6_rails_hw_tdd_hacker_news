f039253253b842c9f54621964743d564298bcee1
Mike Stone <mikestone14@gmail.com>
2014-02-10 18:54:34 -0500

    Created relationship between users and comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  it { should have_many :posts }
+	describe 'relationships' do
+	  it { should have_many :posts }
+	  it { should have_many :comments }
+	end
 end
