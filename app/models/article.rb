5a13f8443e4bdc19248eed207da20c2081601610
Jordan Morano <jordan.morano@gmail.com>
2014-02-10 22:04:46 -0500

    create index view for articles and pass first feature test, viewing article title on root page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :text
+#  url        :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Article < ActiveRecord::Base
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :text
+#  url        :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Article do
