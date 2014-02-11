dadf3e5d45221b58c8317508c85124a274b44661
Becky Bell <rlbell@alum.bu.edu>
2014-02-11 18:58:45 -0500

    added user email to article submission, test passes

--- spec/features/visitor_visits_homepage_spec.rb
+++ spec/features/visitor_visits_homepage_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
-feature 'visitor sees all submissions' do
-	scenario 'on homepage' do
+feature 'visitor lands on site index and' do
+	scenario 'sees all submissions on homepage' do
 	submission = create(:submission)
 		visit submissions_path
 		expect(page).to have_content 'A Wonderful Story'
+	save_and_open_page
 	end
+
+	scenario 'sees users associated with each submission' do
+	submission = create(:submission)
+		visit submissions_path
+		expect(page).to have_content 'becky@email.com'
+	end
+
 end
