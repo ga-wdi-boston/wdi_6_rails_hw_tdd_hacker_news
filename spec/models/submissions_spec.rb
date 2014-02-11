fdbe2de19052fe88772c2349deeb4fad1779ff88
Becky Bell <rlbell@alum.bu.edu>
2014-02-10 19:07:26 -0500

    created databaase, models spec folder, features spec folder

---
+++ spec/models/submissions_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Submission do
+	it 'is invalid without a title' do
+		expect(Submission.create(url: 'http://redditgifts.com/marketplace/collection/valentines-day/?source=slfs-Vdaycooln')).to_not be valid
+	end
+end
