8f6db6e79a115eace217399d35c9b1a49d9ebcdc
Matt Higgins <higginsmt@gmail.com>
2014-02-10 19:29:41 -0500

    Passed second test, sign in existing user is  working

--- spec/features/manage_users_spec.rb
+++ spec/features/manage_users_spec.rb
@@ -11,4 +11,21 @@ feature "Manage Users" do
     expect(page).to have_content('Hacker News')
     save_and_open_page
    end
+
+  scenario "Sign in as an existing user" do
+
+		visit new_user_registration_path
+	  fill_in 'Email', with: "matt@aol.com"
+  	fill_in 'Password', with: "12341234"
+ 		fill_in 'Password confirmation', with: "12341234"
+		click_on 'Sign up'
+		click_on 'Sign out'
+
+	  visit new_user_session_path
+	  fill_in 'Email', with: 'matt@aol.com'
+	  fill_in "Password", with: "12341234"
+	  click_on "Sign in"
+	  expect(page).to have_content('Hacker News')
+	end
+
  end
