d24b28072e8d1054c04ac451cc5a2e7ac0629f73
Laure Fischer <laurefischer27@gmail.com>
2014-02-10 21:26:57 -0500

    installed devise and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 HackerNews::Application.routes.draw do
+  devise_for :users
   resources :submissions
+
+  root to: 'submissions#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Sign in' do
+    scenario "Sign in as an existing user" do
+    visit root_path
+    click_on 'Sign in'
+    visit new_user_session_path
+    expect(page).to have_content("Sign in")
+    fill_in 'Email', with: 'example@gmail.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+  end
+end
