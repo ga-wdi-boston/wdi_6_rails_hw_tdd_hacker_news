6e92e203414549a0dcd0794eb85d10ed26a8d64e
Mike Stone <mikestone14@gmail.com>
2014-02-10 19:03:46 -0500

    Passing tests for user registration and user sign in

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,1 +1,24 @@
 require 'spec_helper'
+
+feature 'Manage Users' do
+	background do
+		user = User.create(email: 'mike@example.com', password: 'password')
+	end
+
+	scenario 'A user should be able to sign up' do
+		visit new_user_registration_path
+		fill_in 'Email', with: 'mike1@example.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password confirmation', with: 'password'
+		click_on 'Sign up'
+		expect(page).to have_content('All Posts')
+	end
+
+	scenario 'A user should be able to sign in' do
+		visit new_user_session_path
+		fill_in 'Email', with: 'mike@example.com'
+		fill_in 'Password', with: 'password'
+		click_on 'Sign in'
+		expect(page).to have_content('All Posts')
+	end
+end
