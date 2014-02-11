07da1f6534945a6fd2c58b0874150c3922f3cd73
Laure Fischer <laurefischer27@gmail.com>
2014-02-11 08:46:27 -0500

    user sign in spec passing

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 feature 'Sign in' do
+    background do
+      User.create(email: 'sixthemail@gmail.com', password: 'password6', password_confirmation: 'password6')
+    end
     scenario "Sign in as an existing user" do
     visit root_path
     click_on 'Sign in'
     visit new_user_session_path
-    fill_in 'Email', with: 'example@gmail.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'Email', with: 'sixthemail@gmail.com'
+    fill_in 'Password', with: 'password6'
     click_on 'Log in'
-    #expect(page).to have_content('Home')
+    expect(page).to have_content('Home')
   end
 end
