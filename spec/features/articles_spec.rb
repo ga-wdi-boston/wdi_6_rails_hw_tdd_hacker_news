4f0f9d0cf5b298063ba16f38cb4937dbac4a0feb
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-10 19:53:55 -0500

    got the feature spec for seeing all articles in article index view to pass (yay)

---
+++ spec/features/articles_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature "a user can see all the articles" do
+	background do
+		Article.create(title: "Star Trek", blurb: "to boldly go...", content: "xxxxxxxxx")
+	end
+
+	scenario 'view all movies as a vistor' do
+		# capybara which will allow you act as a user
+		visit articles_path
+		expect(page).to have_content("Star Trek")
+		expect(page).to have_content("to boldly go...")
+	end
+end
