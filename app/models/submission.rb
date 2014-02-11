3a780e3520d4baeb3f11b3bae4c7e76e6a6caff6
Becky Bell <rlbell@alum.bu.edu>
2014-02-10 20:17:19 -0500

    added validation testing the presence of a user on a submission and made it pass

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ActiveRecord::Base
 	validates  :title, presence: true
 	validates  :url, presence: true, if: "text_block.nil?"
+	validates  :user_id, presence: true
 end
--- spec/models/submissions_spec.rb
+++ spec/models/submissions_spec.rb
@@ -9,4 +9,8 @@ describe Submission do
 	it 'is invalid without either a url or a text block' do
 		expect(Submission.create(title: 'Happy Story')).to_not be_valid
 	end
+
+	it 'has a user' do
+		expect(Submission.create(title: 'A Great Read', url: 'http://publishing.cdlib.org/ucpressebooks/view?docId=ft5q2nb3z7&brand=ucpress')).to_not be_valid
+	end
 end
