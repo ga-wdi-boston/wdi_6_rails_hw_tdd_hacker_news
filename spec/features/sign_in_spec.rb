c48c279d222d8cdfcfad804c8bc23e48f8fa4b24
Laure Fischer <laurefischer27@gmail.com>
2014-02-10 21:48:03 -0500

    seeded database

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,12 +3,11 @@ require 'spec_helper'
 feature 'Sign in' do
     scenario "Sign in as an existing user" do
     visit root_path
-    click_on 'Sign up'
-    visit new_user_registration_path
+    click_on 'Sign in'
+    visit new_user_session_path
     fill_in 'Email', with: 'example@gmail.com'
     fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    click_on 'Submit'
-    expect(page).to have_content('Welcome')
+    click_on 'Log in'
+    expect(page).to have_content('Home')
   end
 end
