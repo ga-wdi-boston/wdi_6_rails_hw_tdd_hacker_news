4eae6bebae76302a603ef25720b8c3906783e51f
Jordan Morano <jordan.morano@gmail.com>
2014-02-10 22:24:05 -0500

    amend article index feature test to look for title and href attribute

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -3,11 +3,21 @@ require 'spec_helper'
 feature 'Manage Articles' do
 
   background do
-    Article.create(title: 'Cats', url: 'http://www.youtube.com/')
+    Article.create(
+      title: 'Richard Lynch, an awesome PHP community guy',
+      url: 'http://richardlynch.blogspot.com/')
+    Article.create(
+      title: 'Facebook Fraud',
+      url: 'http://www.youtube.com/watch?v=oVfHeWTKjag')
+    Article.create(
+      title: 'Mbox',
+      url: 'http://pdos.csail.mit.edu/mbox/')
   end
 
-  scenario 'View all articles as a visitor' do
+  scenario 'View all article titles that are links to an ext webpage' do
     visit root_path
-    expect(page).to have_content('Cats')
+    expect(page).to have_selector "a[href='http://richardlynch.blogspot.com/']", text: 'Richard Lynch, an awesome PHP community guy'
+    expect(page).to have_selector "a[href='http://www.youtube.com/watch?v=oVfHeWTKjag']", text: 'Facebook Fraud'
   end
+
 end
