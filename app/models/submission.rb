fc375bb0e34a80ef0986a240e6f98a2217e9d2df
Laure Fischer <laurefischer27@gmail.com>
2014-02-10 22:02:06 -0500

    added validators for link and description for submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,5 @@
 class Submission < ActiveRecord::Base
   validates :title, presence: true
+  validates :link, presence: true
+  validates :description, presence: true
 end
--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -4,4 +4,12 @@ describe Submission do
   it 'is invalid without a title' do
     expect(Submission.create(link: 'example.com', description: 'This is an interesting article')).to_not be_valid
   end
+
+  it 'is invalid without a link' do
+    expect(Submission.create(title: 'Title', description: 'This is an interesting article')).to_not be_valid
+  end
+
+  it 'is invalid without a description' do
+    expect(Submission.create(title: 'Title', link: 'link.com')).to_not be_valid
+  end
 end
