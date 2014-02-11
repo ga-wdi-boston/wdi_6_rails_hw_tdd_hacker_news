e364abe4e3dc38c2f578fc7d32d1b4f9d6925aac
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-10 22:56:14 -0500

    users can add articles- feature spec passing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,23 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def new
+			@article = Article.new
+		end
+
+		def create
+			@article = Article.new(article_params)
+			@article.save
+			if @article.save
+				redirect_to articles_path
+			else
+				render :new
+			end
+		end
+
+		private
+
+		def article_params
+			params.require(:article).permit(:title, :blurb, :content)
+		end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -19,6 +19,27 @@ feature "sign up a user" do
 		fill_in 'Password', with: 'password'
 		click_on 'Sign In?'
 	end
+
+	scenario 'sign in as a user and add an article' do
+		visit articles_path
+		click_link 'Sign Up'
+		fill_in 'Email', with: 'test3@email.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password confirmation', with: 'password'
+		# save_and_open_page
+		click_on 'Sign Up'
+		visit articles_path
+		click_link 'Add Article'
+		save_and_open_page
+		fill_in 'Title', with: 'Pacific Rim'
+		fill_in 'Blurb', with: 'this was a great movie'
+		fill_in 'Content', with: 'giant robots with neural links to two pilots! fight monsters!'
+		click_on 'Create Article'
+		expect(page).to have_content("All Articles")
+		expect(page).to have_content("Pacific Rim")
+		expect(page).to have_content("great")
+	end
+
 end
 
 
