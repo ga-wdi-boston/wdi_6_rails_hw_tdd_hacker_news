aaa69c8c9285d1f48df48d580bf59bde81454bef
Becky Bell <rlbell@alum.bu.edu>
2014-02-10 19:25:15 -0500

    added validation test for presence of submission title and made test pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140211001901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "submissions", force: true do |t|
+    t.text    "url"
+    t.text    "title"
+    t.integer "user_id"
+  end
+
+  add_index "submissions", ["user_id"], name: "index_submissions_on_user_id", using: :btree
 
 end
--- spec/models/submissions_spec.rb
+++ spec/models/submissions_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Submission do
 	it 'is invalid without a title' do
-		expect(Submission.create(url: 'http://redditgifts.com/marketplace/collection/valentines-day/?source=slfs-Vdaycooln')).to_not be valid
+		expect(Submission.create(url: 'http://redditgifts.com/marketplace/collection/valentines-day/?source=slfs-Vdaycooln')).to_not be_valid
 	end
 end
