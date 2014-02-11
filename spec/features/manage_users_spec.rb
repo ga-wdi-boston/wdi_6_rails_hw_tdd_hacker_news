0450fdc08e18d7fce76c4d81850673629e0f5583
Matt Higgins <higginsmt@gmail.com>
2014-02-10 19:20:24 -0500

    Adding register new user to Manage Users feature testing

---
+++ spec/features/manage_users_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature "Manage Users" do
+
+	scenario "Register a new user" do
+		visit new_user_registration_path
+  	fill_in 'Email', with: "matt@aol.com"
+  	fill_in 'Password', with: "12341234"
+ 		fill_in 'Password confirmation', with: "12341234"
+    click_on 'Sign up'
+    expect(page).to have_content('Hacker News')
+    save_and_open_page
+   end
+ end
