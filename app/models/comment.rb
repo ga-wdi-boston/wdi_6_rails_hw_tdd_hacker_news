c8befb3d27ad4dcb68dc937c55e379be0d001dba
emreiser <emilie.reiser@gmail.com>
2014-02-12 09:10:36 -0500

    Added validation testing for Comment model body field

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :article
   has_many :votes, as: :votable
 
+  validates :body, presence: true
+
   def get_votes
   	self.votes.where(up: true).count
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,10 @@ describe Comment do
   describe 'associations' do
   	it { should belong_to :article }
   end
+
+  describe 'validations' do
+		it 'should be invalid if no body present' do
+			expect(Comment.create).to_not be_valid
+		end
+	end
 end
