cc0fca270553738b26317180d7b6af7b0bfcdd29
tsyan <anna.tsykalova@gmail.com>
2014-02-12 01:25:25 -0500

    added test for 'signed-in user views all posts on front page

--- spec/features/view_posts/user_views_posts_spec.rb
+++ spec/features/view_posts/user_views_posts_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
-feature 'Visitor views all posts on front page' do
-	scenario 'successfully (not signed in)' do
+feature 'User views all posts on front page' do
+	scenario 'successfully (signed in)' do
 		google = create(:google)
 		twitter = create(:twitter)
 		kiva = create(:kiva)
 
+		user = create(:user)
 		visit root_path
+		sign_in_as(user)
 
+		expect(page).to have_content("Signed in as #{user.email}")
 		expect(page).to have_content('I can haz search?')
 		expect(page).to have_content('Microfinance loans to the developing world')
 		expect(page).to have_link('Twitter')
