1222445087365d6718b5e2aea2c209176944ed84
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-12 00:02:04 -0500

    modified migrations - somehow db issues prevented votes from saving

--- db/migrate/20140212002151_create_votes.rb
+++ db/migrate/20140212002151_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.boolean :direction
-      t.belongs_to :votable, polymorphic: true, index: true
+      t.references :votable, polymorphic: true, index: true
 
       t.timestamps
     end
