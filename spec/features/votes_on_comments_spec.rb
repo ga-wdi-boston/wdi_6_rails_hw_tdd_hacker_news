0d99b98b476e149ffb107b358e13533ef6b902e7
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-12 20:42:10 -0500

    votes feature spec now passing for articles and comments

--- spec/features/votes_on_comments_spec.rb
+++ spec/features/votes_on_comments_spec.rb
@@ -1,48 +1,41 @@
 
 
-# require 'spec_helper'
+require 'spec_helper'
 
-# feature "user can up-vote a comment or article" do
-# 	scenario 'user signs in and up-votes a comment' do
-# 			x = create(:article)
-# 			y = Comment.create(content: "do do do do")
-# 			x.comments << y
-# 			user = create(:user)
-# 			visit articles_path
-# 			click_link 'Sign In?'
-# 			fill_in 'Email', with: user.email
-# 			fill_in 'Password', with: user.password
-# 			click_button 'Sign in'
-# 			visit root_path
-# 			save_and_open_page
-# 			click_link 'more'
-# 			click_on 'vote on article'
-# 			expect(page).to have_content 'Vote'
-# 			click_on 'up_vote'
-# 			expect(page).to have_content 'All Articles'
-# 			click_link 'more'
-# 			# save_and_open_page
-# 			# expect(page).to have_content 'up-votes: 1'
-# 		end
+feature "user can up-vote a comment or article" do
+	scenario 'user signs in and up-votes a comment' do
+			x = create(:article)
+			y = Comment.create(content: "do do do do")
+			x.comments << y
+			user = create(:user)
+			visit articles_path
+			click_link 'Sign In?'
+			fill_in 'Email', with: user.email
+			fill_in 'Password', with: user.password
+			click_button 'Sign in'
+			visit root_path
+			click_on 'more'
+			click_on 'see and post comments!'
+			expect(page).to have_content 'vote up on comment'
+			click_link 'vote up on comment'
+			expect(page).to have_content 'up vote.'
+		end
 
-# 		scenario 'user signs in and down-votes a comment' do
-# 			x = create(:article)
-# 			y = Comment.create(content: "do do do do")
-# 			x.comments << y
-# 			user = create(:user)
-# 			visit articles_path
-# 			click_link 'Sign In?'
-# 			fill_in 'Email', with: user.email
-# 			fill_in 'Password', with: user.password
-# 			click_button 'Sign in'
-# 			visit root_path
-# 			click_link 'more'
-# 			click_on 'vote on article'
-# 			expect(page).to have_content 'Vote'
-# 			click_on 'down_vote'
-# 			expect(page).to have_content 'All Articles'
-# 			click_link 'more'
-# 			# save_and_open_page
-# 			# expect(page).to have_content 'up-votes: 1'
-# 		end
-# end
+		scenario 'user signs in and down-votes a comment' do
+			x = create(:article)
+			y = Comment.create(content: "do do do do")
+			x.comments << y
+			user = create(:user)
+			visit articles_path
+			click_link 'Sign In?'
+			fill_in 'Email', with: user.email
+			fill_in 'Password', with: user.password
+			click_button 'Sign in'
+			visit root_path
+			click_on 'more'
+			click_on 'see and post comments!'
+			expect(page).to have_content 'vote down on comment'
+			click_link 'vote down on comment'
+			expect(page).to have_content 'down vote.'
+		end
+end
