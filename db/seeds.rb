96c092d97f047ef45f26de4eb7dac4d9c540c260
emreiser <emilie.reiser@gmail.com>
2014-02-13 13:57:47 -0500

    Added seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |i|
+  User.create!(email: Faker::Internet.email, password: 'password')
+end
+
+30.times do |i|
+  article = Article.create!(title: Faker::Lorem.word, url: Faker::Lorem.word, user: User.find(1) )
+  puts "Created Article #{i}"
+
+  rand(5).times do |i|
+    the_user = User.find(1)
+    article.comments.create!(body: Faker::Lorem.paragraphs(i+1).join(' '), user: the_user)
+    puts "Created comment for #{article.title} "
+  end
+end
