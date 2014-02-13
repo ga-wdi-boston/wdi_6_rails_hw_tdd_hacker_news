94f40867be03dfc6d87c87b8adabe3dbb008c72f
emreiser <emilie.reiser@gmail.com>
2014-02-13 11:22:47 -0500

    Added test to check flash message for submitting comment when not logged in

--- spec/features/user_adds_comment_spec.rb
+++ spec/features/user_adds_comment_spec.rb
@@ -15,4 +15,20 @@ feature 'Logged in user can add a comment' do
 		expect(page).to have_content 'Adding new comment'
 	end
 
+	# scenario 'unsuccessfully if body blank' do
+	# 	save_and_open_page
+	# 	fill_in 'Comment', with: ''
+	# 	click_button 'Add comment'
+	# 	expect(page).to have_content "can't be blank"
+	# end
+
+	scenario 'unsuccessfully if not logged in' do
+		click_link 'Sign out'
+		click_link 'comments'
+		fill_in 'Comment', with: 'Adding new comment'
+		click_button 'Add comment'
+		expect(page).to have_content 'Please sign in or sign up!'
+	end
+
+
 end
