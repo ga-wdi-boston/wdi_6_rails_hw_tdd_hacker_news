f6536ebe31ebaf8fdf5185a2c7683ded9061ddc2
Mike Stone <mikestone14@gmail.com>
2014-02-13 13:56:22 -0500

    Added production database to the database.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 mike = User.create(email: 'mikestone@example.com', password: 'password')
+
+10.times do |i|
+	"user#{i}" = User.create(email: "user#{i}@example.com", password: 'password')
+end
+
+10.times do |i|
+	user = User.find(1)
+	post.create(description: "article#{i}", link: 'http://www.google.com', user_id: user.id)
+end
+
+post = Post.first
+100.times do |i|
+	user = User.find(3)
+	post.comments(body: "Comment number #{i}", user_id: user.id )
