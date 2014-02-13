13b41956a72542669032f9824a7ceeed7c1c6a40
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-13 09:48:50 -0500

    added validation to the articles (with tests)

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :votes, as: :votable
+
+  validates :url, presence: true, unless: :blurb?, format: {with: /https?:/}
+  validates :title, presence: true
+  validates :blurb, presence: true, unless: :url?
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,8 +7,26 @@ describe Article do
   	it { belong_to :user }
   end
 
-  it "is valid with title, blurb, content " do
+  it "is valid with title, blurb, url " do
   	expect(Article.create(title:"Batman",blurb:"Batman!",url:"https://www.google.com/")).to be_valid
   end
 
+  it "is invalid with incorrect url" do
+  	expect(Article.create(title:"Batman",url:"//www.google.com/")).to_not be_valid
+  end
+
+  it "is invalid with without title" do
+  	expect(Article.create(blurb:"Batman!",url:"//www.google.com/")).to_not be_valid
+  end
+
+  it "is valid with without blurb" do
+  	expect(Article.create(title:"Batman", url:"https://www.google.com/")).to be_valid
+  end
+
+   it "is valid with without url" do
+  	expect(Article.create(title:"Batman", blurb:"Nananananana")).to be_valid
+  end
+
+# note - should test the invalid - or not-happy path
+# also should check the the url is a real one
 end
